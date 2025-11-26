7a4992e8a159a92f3876e43ea01b5e68205846f3
manooru <konradklokocki@gmail.com>
2025-11-26 14:32:30 +0100

    Commit

---
+++ app/src/main/java/com/example/libraryview/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.libraryview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+        button.setOnClickListener { //moving to activity id
+            val intent = Intent(this, IDActivity::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener { //closing app
+            finishAffinity()
+            finish()
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/libraryview/Showcase.kt
@@ -0,0 +1,12 @@
+package com.example.libraryview;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+class Showcase : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_showcase)
+    }
+}
