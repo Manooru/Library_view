5b6443070dcb6c8328750f8e61fbd7842b0d71d4
manooru <konradklokocki@gmail.com>
2026-03-11 21:16:28 +0100

    Commit

--- app/src/main/java/com/example/libraryview/Showcase.kt
+++ app/src/main/java/com/example/libraryview/Showcase.kt
@@ -34,7 +34,7 @@ class Showcase : AppCompatActivity() {
                 Error()
             }
         })
-    }
+    }//a
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_showcase)
